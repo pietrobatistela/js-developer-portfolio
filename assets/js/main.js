e498e5b82308e385c5c97e656f083c66372710c6
Pietro <pietromartinsb15@gmail.com>
2025-07-21 15:25:05 -0300

    alterações assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,21 +14,66 @@ function updateProfileInfo(profileData) {
 
   const phone = document.getElementById("profile.phone");
   phone.innerText = profileData.phone;
-  phone.href = `tel:${profileData.phone}`
+  phone.href = `tel:${profileData.phone}`;
 
   const email = document.getElementById("profile.email");
   email.innerText = profileData.email;
-  email.href = `mailito:${profileData.email}`
+  email.href = `mailito:${profileData.email}`;
 }
 
-function updateSoftSkills(profileData){
-    const softSkills = document.getElementById('profile.skills.softSkills')
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById("profile.skills.softSkills");
 
-    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
+}
+
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById("profile.skills.hardSkills");
+
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(
+      (skill) => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`
+    )
+    .join("");
+}
+
+function updateLanguages(profileData){
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <h3 ${project.github ? 'class="title github"' : ''}>${project.name}</h3>
+            <a href="${project.url}" target="_blank"
+            >${project.url}</a>
+        </li>`
+    }).join('')
+}
+
+function updateEducation(profileData){
+    const updateEducation = document.getElementById('profile.education')
+    updateEducation.innerHTML = profileData.education.map(education => {
+        return `
+        <li>
+            <h3 class="title">${education.name}</h3>
+            <p class="period">${education.period}</p>
+            <p>${education.description}</p>
+        </li>`
+    }).join('')
 }
 
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
   updateSoftSkills(profileData);
+  updateHardSkills(profileData);
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
+  updateEducation(profileData);
 })();
